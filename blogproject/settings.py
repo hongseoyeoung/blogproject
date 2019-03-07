571c37e37dc22dc2f0ae84bf4b2bb00411f5b124
hong <yy7062@naver.com>
2019-03-07 21:28:03 +0900

    pleee

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = ')cm^r!8tkgq^ugfajcegbo-z=ojou!@4g-64@e4wr)+3*t8sqc'
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', ')cm^r!8tkgq^ugfajcegbo-z=ojou!@4g-64@e4wr)+3*t8sqc')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
