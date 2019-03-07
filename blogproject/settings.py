2573f2a56c60dee9412d6cc228c3f4094569915b
hong <yy7062@naver.com>
2019-03-07 17:00:44 +0900

    houhoho

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', ')cm^r!8tkgq^ugfajcegbo-z=ojou!
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
 DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
